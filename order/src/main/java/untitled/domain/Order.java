c0b041cb261308552686a014f2f99a8cf8150be5
littleun <38848913+littleun@users.noreply.github.com>
2023-04-15 14:26:37 +0900

    commit

---
+++ order/src/main/java/untitled/domain/Order.java
@@ -0,0 +1,41 @@
+package untitled.domain;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import untitled.OrderApplication;
+import untitled.domain.OrderCancelled;
+import untitled.domain.OrderPlaced;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String customerid;
+
+    private Integer qty;
+
+    private String status;
+
+    @PostPersist
+    public void onPostPersist() {
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+
+        OrderCancelled orderCancelled = new OrderCancelled(this);
+        orderCancelled.publishAfterCommit();
+    }
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = OrderApplication.applicationContext.getBean(
+            OrderRepository.class
+        );
+        return orderRepository;
+    }
+}
